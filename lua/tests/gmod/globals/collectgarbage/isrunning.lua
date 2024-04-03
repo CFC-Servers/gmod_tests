6232597a806bc103cd39b5516dfa8f6523c1f783
Brandon Sturgeon <brandon@brandonsturgeon.com>
2024-04-02 20:29:03 -0700

    Use the new beBetween expectation (#2)
    * Use the new beBetween expectation
    
    * bump
    
    * Refactor x86 specific tests to use the new when/skip feature. Fixes broken x86 tests
    
    * bump

--- lua/tests/gmod/globals/collectgarbage/isrunning.lua
+++ lua/tests/gmod/globals/collectgarbage/isrunning.lua
@@ -3,21 +3,19 @@ return {
     cases = {
         {
             name = "Is not valid on 32bit versions",
+            when = jit.version == "LuaJIT 2.0.4",
             func = function()
-                if jit.version ~= "LuaJIT 2.0.4" then return end
-
                 expect( collectgarbage, "isrunning" ).to.errWith( [[bad argument #1 to '?' (invalid option 'isrunning')]] )
             end
         },
 
         {
-            name = "Is valid on updated JIT versions",
+            name = "Is valid on x86-64",
+            when = jit.version == "LuaJIT 2.1.0-beta3",
             func = function()
-                if jit.version ~= "LuaJIT 2.1.0-beta3" then return end
-
                 expect( collectgarbage, "isrunning" ).to.succeed()
                 expect( collectgarbage( "isrunning" ) ).to.beA( "boolean" )
             end
-        }
+        },
     }
 }
