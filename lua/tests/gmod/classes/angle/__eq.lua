b08f43f2bba6b4908618d02aec7ca2c91b8c71e7
RaphaelIT7 <raphael.i.t@outlook.de>
2024-06-29 07:22:45 +0200

    Add angle metatable methods

---
+++ lua/tests/gmod/classes/angle/__eq.lua
@@ -0,0 +1,28 @@
+
+return {
+    group = "Angle:__div",
+
+    cases = {
+        {
+            name = "Exists on the Angle meta table",
+            func = function()
+                local meta = FindMetaTable( "Angle" )
+                expect( meta.__div ).to.beA( "function" )
+            end
+        },
+
+        {
+            name = "Returns the right value",
+            func = function()
+                local a = Angle( 1, 2, 3 )
+                local b = Angle( 1, 2, 3 )
+                local c = Angle( 4, 5, 6 )
+                local d = Vector( 1, 2, 3 )
+
+                expect( a ).to.equal( b )
+                expect( a ).toNot.equal( c )
+                expect( a ).toNot.equal( d )
+            end
+        },
+    }
+}
---
+++ lua/tests/gmod/classes/angle/__mul.lua
@@ -0,0 +1,26 @@
+
+return {
+    group = "Angle:__mul",
+
+    cases = {
+        {
+            name = "Exists on the Angle meta table",
+            func = function()
+                local meta = FindMetaTable( "Angle" )
+                expect( meta.__mul ).to.beA( "function" )
+            end
+        },
+
+        {
+            name = "Returns the right value",
+            func = function()
+                local a = Angle( 1, 2, 3 )
+                local b = a:__mul( 2 )
+
+                expect( b[1] ).to.equal( 2 )
+                expect( b[2] ).to.equal( 4 )
+                expect( b[2] ).to.equal( 6 )
+            end
+        },
+    }
+}
