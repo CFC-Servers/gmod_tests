9e413d2bcf19712d154cae7f6a574a55e726a7cf
Brandon Sturgeon <brandon@brandonsturgeon.com>
2024-10-25 15:34:21 -0700

    Improve bodytarget tests

--- lua/gmod_tests/sh_init.lua
+++ lua/gmod_tests/sh_init.lua
@@ -26,7 +26,8 @@ if SERVER then
     --- @param class string? The class of the entity
     --- @param model string? The model of the entity
     --- @param shouldSpawn boolean? Whether the entity should be :Spawn()'d
-    MakeTestEntity = function( class, model, shouldSpawn )
+    --- @param shouldFreeze boolean? Whether the entity should be :Freeze()'d (only valid if shouldSpawn is true)
+    MakeTestEntity = function( class, model, shouldSpawn, shouldFreeze )
         shouldSpawn = shouldSpawn == true
 
         local ent = ents.Create( class or "prop_physics" )
@@ -34,15 +35,34 @@ if SERVER then
 
         if shouldSpawn then
             ent:Spawn()
+
+            if shouldFreeze then
+                local physObj = ent:GetPhysicsObject()
+                physObj:EnableMotion( false )
+            end
         end
 
         return ent
     end
 
+    --- @class TestEntityConfig
+    --- @field class string? The class of the entity
+    --- @field model string? The model of the Entity
+    --- @field shouldSpawn boolean? Whether the entity should be :Spawn()'d
+    --- @field shouldFreeze boolean? Whether the entity should be :Freeze()'d (only valid if shouldSpawn is true)
+    --- @field createdCallback fun(ent: Entity)? A callback to run after the entity is created
+
     --- Sets up a testGroup to make a test ent for each test, and remove it after each test
-    WithTestEntity = function( testGroup )
+    --- @param testGroup table The test group to modify
+    --- @param config TestEntityConfig? The configuration for the test entity
+    WithTestEntity = function( testGroup, config )
+        config = config or {}
+
         testGroup.beforeEach = function( state )
-            state.ent = MakeTestEntity()
+            state.ent = MakeTestEntity( config.class, config.model, config.shouldSpawn, config.shouldFreeze )
+
+            local cb = config.createdCallback
+            if cb then cb( state.ent ) end
         end
 
         testGroup.afterEach = function( state )
--- lua/tests/gmod/classes/entity/bodytarget.lua
+++ lua/tests/gmod/classes/entity/bodytarget.lua
@@ -1,11 +1,9 @@
-return WithTestEntity( {
+return {
     groupName = "Entity:BodyTarget",
 
-    -- NOTE:
-    -- Using the noise parameter appears to adjust the output by approximately:
-    -- x: -3.5698315059562e-07 - 3.571777824618e-07 (hardly any adjustment)
-    -- y: 0 - 0 (no adjustment)
-    -- z: -8.4222898483276 - 8.4268808364868
+    afterEach = function( state )
+        SafeRemoveEntity( state.ent )
+    end,
 
     cases = {
         {
@@ -16,51 +14,120 @@ return WithTestEntity( {
             end
         },
 
+        -- When operating on prop_physics
         {
-            name = "Returns the expected vector",
-
+            name = "Always returns a prop's WorldSpaceCenter",
             func = function( state )
-                local ent = state.ent
-                local origin = Vector( 100, 200, 300 )
+                local ent = MakeTestEntity( "prop_physics", "models/props_c17/oildrum001.mdl", true, true )
+                state.ent = ent
 
-                local output = ent:BodyTarget( origin, false )
-                local x, y, z = output:Unpack()
+                local expected = ent:WorldSpaceCenter()
 
-                expect( x ).to.aboutEqual( 0 )
-                expect( y ).to.equal( 0 )
-                expect( z ).to.aboutEqual( 8.4375 )
+                do
+                    local output = ent:BodyTarget( Vector( -1000, 5000, 0 ), false )
+                    local x, y, z = output:Unpack()
+
+                    expect( x ).to.equal( expected.x )
+                    expect( y ).to.equal( expected.y )
+                    expect( z ).to.equal( expected.z )
+                end
+
+                do
+                    local output = ent:BodyTarget( Vector( 1000, -5000, 500 ), false )
+                    local x, y, z = output:Unpack()
+
+                    expect( x ).to.equal( expected.x )
+                    expect( y ).to.equal( expected.y )
+                    expect( z ).to.equal( expected.z )
+                end
             end
         },
-
         {
-            name = "Returns a noisy vector when noise is enabled",
+            name = "Noisy parameter does nothing for props",
             func = function( state )
-                local ent = state.ent
-                local origin = Vector( 100, 200, 300 )
+                local ent = MakeTestEntity( "prop_physics", "models/props_c17/oildrum001.mdl", true, true )
+                state.ent = ent
+
+                local origin = Vector( 100, 100, 0 )
 
                 local noisyOutput = ent:BodyTarget( origin, true )
                 local cleanOutput = ent:BodyTarget( origin, false )
 
-                local difference = noisyOutput - cleanOutput
-                local x, y, z = difference:Unpack()
-
-                expect( x ).notTo.equal( 0 )
-                expect( y ).to.equal( 0 ) -- Noise is not applied to the Y value
-                expect( z ).notTo.equal( 0 )
+                expect( noisyOutput:IsEqualTol( cleanOutput, 0 ) ).to.beTrue()
             end
         },
 
+        -- NOTE:
+        -- This 12.5 offset seems to apply to many NPCs
         {
-            name = "Defaults noisy parameter to false",
+            name = "Returns WorldSpaceCenter with a slight Z offset for npc_gman",
             func = function( state )
-                local ent = state.ent
-                local origin = Vector( 100, 200, 300 )
+                local ent = MakeTestEntity( "npc_gman", "", true )
+                state.ent = ent
 
-                local implied = ent:BodyTarget( origin )
-                local implicit = ent:BodyTarget( origin, false )
+                local worldSpaceCenter = ent:WorldSpaceCenter()
+                local output = ent:BodyTarget( Vector( 0, 0, 0 ), false )
 
-                expect( implied ).to.equal( implicit )
+                expect( output.x ).to.equal( worldSpaceCenter.x )
+                expect( output.y ).to.equal( worldSpaceCenter.y )
+                expect( output.z ).to.aboutEqual( worldSpaceCenter.z + 12.5 )
             end
-        }
+        },
+        {
+            name = "Noisy parameter applies Z-axis noise for npc_gman",
+            func = function( state )
+                local ent = MakeTestEntity( "npc_gman", "", true )
+                state.ent = ent
+
+                local cleanOutput = ent:BodyTarget( Vector( 0, 0, 0 ), false )
+                local noisyOutput = ent:BodyTarget( Vector( 0, 0, 0 ), true )
+
+                local difference = noisyOutput - cleanOutput
+
+                expect( difference.x ).to.equal( 0 )
+                expect( difference.y ).to.equal( 0 )
+                expect( difference.z ).notTo.equal( 0 )
+            end
+        },
+
+        -- Zombies have a 9.5 offset
+        {
+            name = "Returns WorldSpaceCenter with a slight Z offset for npc_zombie",
+            func = function( state )
+                local ent = MakeTestEntity( "npc_zombie", "", true ) --[[@as NPC]]
+                state.ent = ent
+
+                local worldSpaceCenter = ent:WorldSpaceCenter()
+                local output = ent:BodyTarget( Vector( 0, 0, 0 ), false )
+
+                expect( output.x ).to.equal( worldSpaceCenter.x )
+                expect( output.y ).to.equal( worldSpaceCenter.y )
+                expect( output.z ).to.equal( worldSpaceCenter.z + 9.5 )
+            end
+        },
+        {
+            name = "Returns a special calculation for flinching npc_zombie",
+            func = function( state )
+                local ent = MakeTestEntity( "npc_zombie", "", true ) --[[@as NPC]]
+                state.ent = ent
+
+                ent:SetSchedule( SCHED_BIG_FLINCH )
+                local pos = ent:GetPos()
+                local headTarget = ent:HeadTarget( Vector( 0, 0, 0 ) )
+                local expectedZ = (pos.z + headTarget.z) / 2
+
+                local worldSpaceCenter = ent:WorldSpaceCenter()
+                local output = ent:BodyTarget( Vector( 0, 0, 0 ), false )
+
+                expect( output.x ).to.equal( worldSpaceCenter.x )
+                expect( output.y ).to.equal( worldSpaceCenter.y )
+                expect( output.z ).to.equal( expectedZ )
+            end
+        },
+
+        -- TODO: Special cases for Crows
+        -- TODO: Special cases for Combine
+        -- TODO: Special cases for Striders
+        -- TODO: Special cases for Players in Vehicles
     }
-} )
+}
