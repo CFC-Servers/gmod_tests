258956a86d006129e65114dcb6620085327d2690
Brandon Sturgeon <brandon@brandonsturgeon.com>
2025-04-26 12:19:42 -0700

    Update failure reporting

---
+++ .github/tools/parse_test_failures.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+def main():
+    parsing_active = False
+
+    try:
+        for line in sys.stdin:
+            line = line.rstrip()
+
+            if line == "Test failures:":
+                parsing_active = True
+                continue
+
+            if not parsing_active:
+                continue
+
+            if line.startswith("=== "):
+                print("\n")
+                print(line)
+            elif line.startswith("FAIL ["):
+                print(line)
+            elif line.startswith("engine.CloseServer has beeen called!"):
+                return
+
+    except Exception as e:
+        print(f"Python script error: {e}", file=sys.stderr)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
+
