e0eaf5c3e5e3711dca1cdc3f0863aa26c784d312
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2025-07-01 14:11:22 -0400

    Add string.Left/string.Right

---
+++ lua/tests/gmod/unit/libraries/string/Left.lua
@@ -0,0 +1,26 @@
+return {
+    groupName = "string.Left",
+
+    cases = {
+        {
+            name = "Exists on the string table",
+            func = function()
+                expect( string.Left ).to.beA( "function" )
+            end
+        },
+
+        {
+            name = "Returns the expected string value",
+            func = function()
+                expect( string.Left( "Hello!", 1 ) ).to.equal( "H" )
+                expect( string.Left( "H", 1 ) ).to.equal( "H" )
+                expect( string.Left( "Hello!", 10 ) ).to.equal( "Hello!" )
+                expect( string.Left( nil ) ).to.errWith( [[bad argument #1 to 'Left']] )
+                expect( string.Left( nil, 1 ) ).to.errWith( [[bad argument #1 to 'Left']] )
+                expect( string.Left( "Hello!" ) ).to.equal( "Hello!" )
+                expect( string.Left( "Hello!", -1 ) ).to.equal( "Hello!" )
+                expect( string.Left( "garrys mod", 5 ) ).to.equal( "garry" )
+            end
+        }
+    }
+}
