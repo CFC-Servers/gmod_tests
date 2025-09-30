1517764fafd6fa53985fc98eb95af91f9ea47b9f
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2025-09-29 23:38:08 -0400

    Fix tests

--- lua/tests/gmod/unit/libraries/string/Left.lua
+++ lua/tests/gmod/unit/libraries/string/Left.lua
@@ -15,10 +15,11 @@ return {
                 expect( string.Left( "Hello!", 1 ) ).to.equal( "H" )
                 expect( string.Left( "H", 1 ) ).to.equal( "H" )
                 expect( string.Left( "Hello!", 10 ) ).to.equal( "Hello!" )
-                expect( string.Left( nil ) ).to.errWith( [[bad argument #1 to 'Left']] )
-                expect( string.Left( nil, 1 ) ).to.errWith( [[bad argument #1 to 'Left']] )
+                expect( string.Left( nil ) ).to.errWith( [[bad argument #1 to 'Left' (string expected, got nil)]] )
+                expect( string.Left( nil, 1 ) ).to.errWith( [[bad argument #1 to 'Left' (string expected, got nil)]] )
                 expect( string.Left( "Hello!" ) ).to.equal( "Hello!" )
                 expect( string.Left( "Hello!", -1 ) ).to.equal( "Hello!" )
+                expect( string.Left( "Hello World!", -8 ) ).to.equal( "Hello" )
                 expect( string.Left( "garrys mod", 5 ) ).to.equal( "garry" )
             end
         }
--- lua/tests/gmod/unit/libraries/string/Right.lua
+++ lua/tests/gmod/unit/libraries/string/Right.lua
@@ -15,10 +15,11 @@ return {
                 expect( string.Right( "Hello!", 1 ) ).to.equal( "!" )
                 expect( string.Right( "H", 1 ) ).to.equal( "H" )
                 expect( string.Right( "Hello!", 10 ) ).to.equal( "Hello!" )
-                expect( string.Right( nil ) ).to.errWith( [[attempt to perform arithmetic on local 'num']] )
-                expect( string.Right( nil, 1 ) ).to.errWith( [[bad argument #1 to 'Right']] )
-                expect( string.Right( "Hello!" ) ).to.errWith( [[attempt to perform arithmetic on local 'num']] )
+                expect( string.Right( nil ) ).to.errWith( [[attempt to perform arithmetic on local 'num' (a nil value)]] )
+                expect( string.Right( nil, 1 ) ).to.errWith( [[bad argument #1 to 'Right' (string expected, got nil)]] )
+                expect( string.Right( "Hello!" ) ).to.errWith( [[attempt to perform arithmetic on local 'num' (a nil value)]] )
                 expect( string.Right( "Hello!", -1 ) ).to.equal( "Hello!" )
+                expect( string.Right( "Hello World!", -8 ) ).to.equal( "orld!" )
                 expect( string.Right( "garrys mod", 3 ) ).to.equal( "mod" )
             end
         }
