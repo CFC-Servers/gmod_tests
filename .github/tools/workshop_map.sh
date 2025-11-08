71f8ff0169211d6a58eadd9d5f74cfe8824872a0
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2025-11-08 21:58:54 +0100

    Use absolute path

--- .github/tools/workshop_map.sh
+++ .github/tools/workshop_map.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Fix our path since were at project/.github/tools/
-cd ../../../
-
 # Setup HolyTest
-wget -q https://github.com/RaphaelIT7/gmod-holytest/releases/latest/download/linux.tar.gz -O gluatest/docker/_gluatest_artifacts/linux.tar.gz
+wget -q https://github.com/RaphaelIT7/gmod-holytest/releases/latest/download/linux.tar.gz -O ../../../gluatest/docker/_gluatest_artifacts/linux.tar.gz
 
 # Workshop map since the workshop itself frequently fails inside the containers
 wget -O gm_glua_tests.bsp https://github.com/CFC-Servers/gm_tests_map/raw/refs/heads/main/maps/gm_glua_tests.bsp
 mkdir -p garrysmod/maps
 mv gm_glua_tests.bsp garrysmod/maps/
-tar -czf gluatest/docker/_gluatest_artifacts/gm_glua_tests.tar.gz -C . garrysmod
+tar -czf ../../../gluatest/docker/_gluatest_artifacts/gm_glua_tests.tar.gz -C . garrysmod
 rm -r garrysmod
